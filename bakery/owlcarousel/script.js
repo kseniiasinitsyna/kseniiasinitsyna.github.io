13752312718e40f3fc7481a412a6b3d19bbdc050
kseniiasinitsyna <47609222+kseniiasinitsyna@users.noreply.github.com>
2019-03-06 22:40:26 +0300

    Add files via upload

---
+++ bakery/owlcarousel/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+            $(".owl-carousel").owlCarousel({
+                items:1,
+                loop: true,
+                dots: false,
+                nav: true,
+                navText : ['',''],
+                smartSpeed: 800,
+                animateOut: 'fadeOut',
+                animateIn: 'fadeIn'             
+            });
+        });
+$(document).ready(function(){   
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+});
